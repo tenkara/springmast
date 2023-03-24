9864877dffefcc6170de7c9910498d29c9052492
Tenkara <rajknakka@gmail.com>
2023-03-23 21:21:49 -0600

    add base url

--- 1-springboot-rest-api/src/main/java/tenkara/springbootrestapi/controller/StudentController.java
+++ 1-springboot-rest-api/src/main/java/tenkara/springbootrestapi/controller/StudentController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import tenkara.springbootrestapi.bean.Student;
 
 @RestController
+// @RequestMapping("/student")
 public class StudentController {
 
     // http://localhost:8080/student
